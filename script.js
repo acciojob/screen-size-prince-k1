baa24db81c2491a070f55eddb8a0a70c854c3e57
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-26 20:21:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const h = document.querySelector('#h');
+
+function updateSize() {
+	let width = window.innerWidth;
+	let height = window.innerHeight;
+	h.innerText = `Width: ${width} and Height: ${height}`;
+}
+updateSize();
+window.addEventListener('resize', updateSize);
