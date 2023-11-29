4aaa5ed73431d73714cd66468cf49bf68fec2730
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-29 11:00:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-const h = document.querySelector('#sizeInfo');
+const h = document.querySelector('h1');
 
 function updateSize() {
 	let width = window.innerWidth;
